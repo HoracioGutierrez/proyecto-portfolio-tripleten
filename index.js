9f23b9e65e12e41a89c0c587ce77832bc68edbd3
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-07-23 22:17:29 -0300

    Add new style to hero section and prevent link clicking

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const menuButton = document.querySelector('.hero__button');
 const closeButton = document.querySelector('.sidebar__button');
 const sidebar = document.querySelector('.sidebar');
+const links = document.querySelectorAll('.sidebar__link');
+
+
+links.forEach((item) => {
+  item.addEventListener("click", (evt) => {
+    /* evt.preventDefault() */
+  })
+})
 
 menuButton.addEventListener("click", () => {
   sidebar.classList.add("sidebar_open")
@@ -9,3 +17,8 @@ menuButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
   sidebar.classList.remove("sidebar_open")
 })
+
+lucide.createIcons();
+
+//SPA : Single Page Application
+//fetch()
